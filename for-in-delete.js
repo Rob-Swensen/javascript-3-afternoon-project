6e1208601974ccc67ee313a7fe532e7ea2af8804
Rob-Swensen <rg.swensen801@gmail.com>
2020-02-14 17:15:33 -0700

    Second Commit

--- for-in-delete.js
+++ for-in-delete.js
@@ -39,8 +39,12 @@
   Inside the function showValues, write a for in loop that concatenates each of the property values and returns the concatenated string.
 */
 
-function showValues( obj ) {
-  //Code Here
+function showValues(obj) {
+  let string = '';
+  for(let key in obj){
+    string = string + obj[key]
+  }
+  return string;
 }
 
 
@@ -54,7 +58,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10(obj){
+  for(let key in obj){
+    if(obj[key] > 10){
+      obj[key] = 0
+    }
+  }
+  return obj;
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,7 +77,12 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj){
+  for(let key in obj){
+    obj[key] *= 2
+  }
+    return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -80,7 +96,15 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function secrets(obj){
+  let newString = '';
+  for(let key in obj){
+    if(key.startsWith('sh')){
+      newString += obj[key]
+    }
+  }
+  return newString;
+}
 
 
 /* 
@@ -111,7 +135,11 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+ 
+function removePassword(obj){
+  delete obj.password;
+  return obj;
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -130,7 +158,11 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+for(let key in deleteTheBigNumbers){
+  if(deleteTheBigNumbers[key] > 100){
+    delete deleteTheBigNumbers[key];
+  }  
+}
 
 
 ////////// PROBLEM 7 //////////

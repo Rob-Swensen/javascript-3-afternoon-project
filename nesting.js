c2f98788b972b9bfc58d2879c8b5d93bb7a4c503
Rob-Swensen <rg.swensen801@gmail.com>
2020-02-16 21:51:42 -0700

    Spread Commit

--- nesting.js
+++ nesting.js
@@ -177,6 +177,4 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
-
-
+// Code Here
--- spread.js
+++ spread.js
@@ -18,6 +18,7 @@ let product = {
 }
 
 // code here
+let productCopy = [...product];
 
 
 
@@ -29,6 +30,8 @@ let product = {
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 // code here
+
+let colorsCopy = [...colors]
 
 
 
